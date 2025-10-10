a8c52089614ba49b04a3cbd54661df9feb64ef27
Roman <romaproffen@yandex.ru>
2025-10-10 15:57:20 +0300

    feat: logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,8 +1,18 @@
+//go:build !go1.21
+
 package main
 
 import (
 	"fmt"
+	"golang.org/x/exp/slog"
+	"os"
 	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -11,10 +21,40 @@ func main() {
 
 	fmt.Println(cfg)
 
-	// TODO: init logger (slog)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting app",
+		slog.String("env", cfg.Env),
+		slog.Any("config", cfg),
+		slog.Int("port", cfg.GRPC.Port),
+	)
+
+	log.Debug("debug message")
+
+	log.Error("error message")
 
 	// TODO: init app
 
 	// TODO: run gRPC server
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
